1b56424d649b71f47012524e4cf715b6d9eefd9f
trentteinert <102979377+trentteinert@users.noreply.github.com>
2022-05-11 11:33:12 -0400

    init

--- src/App.js
+++ src/App.js
@@ -12,6 +12,18 @@ import Model005page from './model005';
 import Model006page from './model006';
 import Model007page from './model007';
 import Model008page from './model008';
+import Model009page from './model009';
+import Model010page from './model010';
+import Model011page from './model011';
+import Model012page from './model012';
+import Model013page from './model013';
+import Model014page from './model014';
+import Model015page from './model015';
+import Model016page from './model016';
+import Model017page from './model017';
+import Model018page from './model018';
+import Model019page from './model019';
+import Model020page from './model020';
 import Info from './info';
 import {Route, Routes} from 'react-router-dom'
 
@@ -30,6 +42,18 @@ function App() {
       <Routes><Route path='/model006' element={<Model006page />}/></Routes>
       <Routes><Route path='/model007' element={<Model007page />}/></Routes>
       <Routes><Route path='/model008' element={<Model008page />}/></Routes>
+      <Routes><Route path='/model009' element={<Model009page />}/></Routes>
+      <Routes><Route path='/model010' element={<Model010page />}/></Routes>
+      <Routes><Route path='/model011' element={<Model011page />}/></Routes>
+      <Routes><Route path='/model012' element={<Model012page />}/></Routes>
+      <Routes><Route path='/model013' element={<Model013page />}/></Routes>
+      <Routes><Route path='/model014' element={<Model014page />}/></Routes>
+      <Routes><Route path='/model015' element={<Model015page />}/></Routes>
+      <Routes><Route path='/model016' element={<Model016page />}/></Routes>
+      <Routes><Route path='/model017' element={<Model017page />}/></Routes>
+      <Routes><Route path='/model018' element={<Model018page />}/></Routes>
+      <Routes><Route path='/model019' element={<Model019page />}/></Routes>
+      <Routes><Route path='/model020' element={<Model020page />}/></Routes>
       <Routes><Route path='/upload' element={<Upload />}/></Routes>
       <Routes><Route path='/information' element={<Info />}/></Routes>
     </div>
---
+++ src/model019.js
@@ -0,0 +1,43 @@
+import './model019.css';
+import { Suspense, useCallback } from "react";
+import React from "react"
+import styled from "styled-components";
+import { useNavigate } from 'react-router-dom';
+
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Model019 from './components/Model019';
+
+export default function Model019page() {
+
+  const navigate = useNavigate();
+  const handleOnClick = useCallback(() => navigate('/inside', {replace: true}), [navigate]);
+ 
+  return (
+    <Wrapper className="App">
+      <Canvas clasName="canvas" camera={{ fov: 40, position: [2.2, 2, -2] }}>
+        <OrbitControls target={[0, 0, 0]} />
+        <ambientLight intensity={1} />
+        <spotLight position={[3, 5, 1]} angle={0.3} />
+        <Suspense fallback={null}>
+          <Model019 position={[0, 0, 0]} />
+        </Suspense>
+      </Canvas>
+        <div>
+        <button id='Button4' onClick={handleOnClick}>
+        back
+      </button>
+        </div>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  position: relative;
+  background: #666666;
+
+  canvas {
+    height: 850px;
+    width: 100%;
+  }
+`;
